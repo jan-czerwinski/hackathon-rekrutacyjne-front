03ec02c229e8a280b9de096a0438b1ee45c554a5
maciek <macieq2048@gmail.com>
2021-12-28 17:41:13 +0100

    update cloud function

--- api/cloud_function.py
+++ api/cloud_function.py
@@ -2,13 +2,14 @@ import io
 import numpy as np
 from PIL import Image
 
-from flask import Flask, send_file, make_response
+from flask import send_file, make_response
 
 
-def read_image(path) -> np.ndarray:
+def read_image(path: str) -> np.ndarray:
     img = Image.open(path)
     img = np.asarray(img.__array__())
     return img
+
 
 def pad_array(img: np.ndarray, pad: int) -> np.ndarray:
     """3D convolution by sub-matrix summing.
@@ -48,15 +49,13 @@ def convolve(img: np.ndarray, kernel: np.ndarray, pad: int = 0) -> np.ndarray:
     return result
 
 
-
-
 class EdgeDetector:
     def __init__(self, img: np.ndarray,
                  gauss_size: int = 5,
                  sigma: float = 1,
                  rgb_weights: tuple = (0.2989, 0.5870, 0.1140),
                  threshold_values: tuple = (0.05, 0.2),
-                 edges_vals: tuple = (80, 200), ) -> None:
+                 edges_vals: tuple = (80, 255), ) -> None:
         self.img = img
         self.size = gauss_size
         self.sigma = sigma
@@ -71,7 +70,7 @@ class EdgeDetector:
         return g
 
     def intensity_gradient(self, img: np.ndarray):
-        sobel_filter_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]], np.float32)
+        sobel_filter_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
         sobel_filter_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
 
         gx = convolve(img, sobel_filter_x)
@@ -82,7 +81,7 @@ class EdgeDetector:
         # normalization
         g = g / g.max() * 255
 
-        theta = np.arctan2(gx, gy)
+        theta = np.arctan2(gy, gx)
 
         return g, theta
 
@@ -99,19 +98,15 @@ class EdgeDetector:
                     q = 255
                     r = 255
 
-                    # angle 0
                     if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
                         q = src_img[i, j + 1]
                         r = src_img[i, j - 1]
-                    # angle 45
                     elif 22.5 <= angle[i, j] < 67.5:
                         q = src_img[i + 1, j - 1]
                         r = src_img[i - 1, j + 1]
-                    # angle 90
                     elif 67.5 <= angle[i, j] < 112.5:
                         q = src_img[i + 1, j]
                         r = src_img[i - 1, j]
-                    # angle 135
                     elif 112.5 <= angle[i, j] < 157.5:
                         q = src_img[i - 1, j - 1]
                         r = src_img[i + 1, j + 1]
@@ -141,6 +136,31 @@ class EdgeDetector:
 
         return out_img
 
+    def edge_tracking(self, src_img: np.ndarray) -> np.ndarray:
+        src_width, src_height = src_img.shape
+
+        out_img = src_img.copy()
+
+        for i in range(1, src_width - 1):
+            for j in range(1, src_height - 1):
+                if (src_img[i, j] == self.weak_edge_val):
+                    try:
+                        if ((src_img[i + 1, j - 1] == self.strong_edge_val)
+                                or (src_img[i + 1, j] == self.strong_edge_val)
+                                or (src_img[i + 1, j + 1] == self.strong_edge_val)
+                                or (src_img[i, j - 1] == self.strong_edge_val)
+                                or (src_img[i, j + 1] == self.strong_edge_val)
+                                or (src_img[i - 1, j - 1] == self.strong_edge_val)
+                                or (src_img[i - 1, j] == self.strong_edge_val)
+                                or (src_img[i - 1, j + 1] == self.strong_edge_val)):
+                            out_img[i, j] = self.strong_edge_val
+                        else:
+                            out_img[i, j] = 0
+                    except IndexError as e:
+                        pass
+
+        return out_img
+
     # img as ndarray with shape (height, width, channels)
     def edge_detection(self, img: np.ndarray) -> np.ndarray:
 
@@ -148,7 +168,7 @@ class EdgeDetector:
         # filtered_image should be shape (height, width)
         filtered_image = np.dot(img[..., :3], self.rgb_weights)
 
-        # apllying gaussian kernel with shape (5,5)
+        # applying gaussian kernel with shape (5,5)
         filtered_image = convolve(filtered_image, self.gaussian_kernel(size=self.size, sigma=self.sigma))
 
         # finding intensity gradient
@@ -158,6 +178,8 @@ class EdgeDetector:
 
         filtered_image = self.double_treshold(filtered_image, self.low_threshold, self.high_threshold)
 
+        filtered_image = self.edge_tracking(filtered_image)
+
         return filtered_image
 
     def detect(self) -> np.ndarray:
@@ -165,40 +187,18 @@ class EdgeDetector:
 
 
 def process_image(request):
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600',
-            'Access-Control-Allow-Credentials': 'true'
-        }
-
-        return ('', 204, headers)
-
     file = request.files['image']
 
     img = read_image(file.stream)
     detector = EdgeDetector(img)
     filtered_img = detector.detect()
 
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST'
-    }
-
     fp = io.BytesIO()
     format = Image.registered_extensions()['.jpg']
     Image.fromarray(filtered_img).convert('L').save(fp, format)
 
-
     resp = make_response(send_file(io.BytesIO(fp.getvalue()),
-                     attachment_filename='asdasd.png',
-                     mimetype='image/png'))
-    resp.headers = headers
+                                   attachment_filename='asdasd.png',
+                                   mimetype='image/png'))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
-
