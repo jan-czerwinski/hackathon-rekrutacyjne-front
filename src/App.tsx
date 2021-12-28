a14551e4bf95ee5ce8a331b3a5f43610961b6e1d
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2021-12-28 18:35:16 +0100

    final touches; css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import RingLoader from 'react-spinners/RingLoader';
+import CSS from 'csstype';
 
 function App() {
   const [inputImgSrc, setInputImgSrc] = useState<string>();
   const [outputImgSrc, setOutputImgSrc] = useState<string>();
   const [isDetectingEdges, setIsDetectingEdges] = useState(false);
   const inputFileRef = useRef<HTMLInputElement>(null);
-  const inputImgRef = useRef<HTMLImageElement>(null);
-  const [imgTranslateCss, setImgTranslateCss] =
-    useState<string>('-translateY(0px)');
+  const [edgeDetectionTime, setEdgeDetectionTime] = useState(0);
 
-  useEffect(() => {
-    //check file dimensions and translate image to center it verically
-    //i really couldn't find another way lol
-    inputImgRef.current?.height &&
-      setImgTranslateCss(
-        `translateY(-${Math.round(inputImgRef.current?.height / 2)}px)`
-      );
-  }, [inputImgRef.current?.height]);
-
-  const onFileChange = () => {
+  const onInputFileChange = () => {
     if (!inputFileRef.current?.files) return;
     const file = inputFileRef.current.files[0];
     const fileUrl = URL.createObjectURL(file);
+    setOutputImgSrc(undefined);
     setInputImgSrc(fileUrl);
   };
 
   const detectEdges = async () => {
     setIsDetectingEdges(true);
+    const startTime = performance.now();
 
     const detectEdgesCloudFunctionUrl =
       'https://europe-central2-optimistic-host-320114.cloudfunctions.net/function-1';
@@ -38,8 +30,7 @@ function App() {
     const formData = new FormData();
     formData.append('image', file);
 
-    console.log('fetching.... ');
-    const response = await fetch('http://127.0.0.1:5000/im_size', {
+    const response = await fetch(detectEdgesCloudFunctionUrl, {
       method: 'POST',
       body: formData,
     });
@@ -49,7 +40,9 @@ function App() {
     const responseImgUrl = URL.createObjectURL(await response.blob());
 
     setIsDetectingEdges(false);
+    const endTime = performance.now();
 
+    setEdgeDetectionTime(endTime - startTime);
     setOutputImgSrc(responseImgUrl);
   };
 
@@ -57,37 +50,31 @@ function App() {
     <div className="w-screen h-screen p-12 bg-blue-200">
       <div className="w-full h-full flex flex-col justify-between">
         <div className="flex justify-around w-full h-4/5 my-auto">
-          <div className="w-1/2 h-full">
-            {inputImgSrc && imgTranslateCss && (
+          <div className="w-1/2 h-full flex">
+            {inputImgSrc && (
               <img
-                className="relative top-1/2 mx-auto w-auto h-auto max-h-full object-contain shadow-md p-4 bg-white transform"
+                className="m-auto w-auto h-auto max-h-full object-contain shadow-md p-4 bg-white"
                 src={inputImgSrc}
-                ref={inputImgRef}
-                style={{
-                  transform: imgTranslateCss,
-                }}
-                alt=""
               />
             )}
           </div>
-          <div className="w-1/2 h-full">
-            <div className="h-full grid place-content-center">
-              {/* <RingLoader color="blue" size={120} loading={isDetectingEdges} /> */}
-              <img
-                className="relative top-1/2 mx-auto w-auto h-auto max-h-full object-contain shadow-md p-4 bg-white transform"
-                src={outputImgSrc}
-                style={{
-                  transform: imgTranslateCss,
-                }}
-                alt=""
-              />
-            </div>
+          <div className="w-1/2 h-full flex justify-center items-center">
+            {isDetectingEdges ? (
+              <RingLoader color="blue" size={120} />
+            ) : (
+              outputImgSrc && (
+                <img
+                  className="m-auto w-auto h-auto max-h-full object-contain shadow-md p-4 bg-white"
+                  src={outputImgSrc}
+                />
+              )
+            )}
           </div>
         </div>
 
-        <div className="flex h-1/5 justify-around text-white text-xl">
+        <div className="flex h-1/5 justify-around text-white text-3xl">
           <input
-            onChange={onFileChange}
+            onChange={onInputFileChange}
             type="file"
             ref={inputFileRef}
             accept=".png, .jpg, .jpeg"
@@ -97,15 +84,22 @@ function App() {
             className="m-6 w-1/4 rounded-xl bg-blue-300 shadow-md "
             onClick={() => inputFileRef.current?.click()}
           >
-            upload..
+            Upload png/jpg/jpeg image
           </button>
-
-          <button
-            className="m-6 w-1/4 rounded-xl bg-blue-300 shadow-md"
-            onClick={detectEdges}
-          >
-            detect edges
-          </button>
+          {outputImgSrc ? (
+            <span className="grid place-content-center text-center w-1/4">
+              Total time: <br />
+              {edgeDetectionTime} ms
+            </span>
+          ) : (
+            <button
+              className="m-6 w-1/4 rounded-xl bg-blue-300 shadow-md"
+              onClick={detectEdges}
+              disabled={isDetectingEdges}
+            >
+              Detect edges
+            </button>
+          )}
         </div>
       </div>
     </div>
